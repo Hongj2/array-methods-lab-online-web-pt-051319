5da619e1835fb081905a2679bcd8561b6414e2f3
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-05-15 04:42:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,34 +1,23 @@
-def instantiate_new_array 
-  []
+def using_include(array, element)
 
 end
 
-def array_with_two_elements 
-  [1, 2]
+def using_sort(array)
+
 end
 
-def first_element (array)
-  array [0]
+def using_reverse(array)
+
 end
 
+def using_first(array)
 
-def third_element (array)
-  array [2]
 end
 
+def using_last(array)
 
-def last_element (array)
-  array [-1]
 end
 
-def first_element_with_array_methods (array)
-  array.first
-end
+def using_size(array)
 
-def last_element_with_array_methods (array)
-  array.last
-end
-
-def length_of_array (array)
-  array.length
 end
